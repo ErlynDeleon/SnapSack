fa074651efd6a6fb2586099040bdfac155e758dd
Quin <erlynqueendeleon81@gmail.com>
2024-03-11 19:22:56 +0800

    updated CustHome navigantionpanel

---
+++ Windows/ProductWindow.java
@@ -0,0 +1,24 @@
+package Windows;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ProductWindow extends JFrame{
+  JLabel label = new JLabel();
+  ProductWindow(){
+    this.add(label);
+        ImageIcon icon = new ImageIcon("Windows\\pictures\\1-removebg-preview.png");
+        this.setIconImage(icon.getImage());
+        this.getContentPane().setBackground(new Color(129, 104, 157));
+        this.setTitle("SnapSack");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(1500, 900);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+  }
+}
